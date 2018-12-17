60f219afb15061554a0e59e2462bdf2d59c5140f
Garreau Arthur <arthurgarreau.web@gmail.com>
2018-12-17 23:49:48 +0100

    Adds script to create the tables of the database

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # webProjetInfra
+
 Partie web/php du projet infra/java/web intro i2 ESEO
+
+
+## Postgresql
+
+Avec `psql`, créer une base de données :
+
+	CREATE DATABASE hothell;
+
+Lister base de données :
+
+	\l
+
+Utiliser un base de données :
+
+	\connect hothell
+
+Lister tables :
+
+	\dt
+
+Décrire un table :
+
+	\d+ tablename
---
+++ create.sql
@@ -0,0 +1,65 @@
+# Postgresql ne gère pas la case, sauf si on entoure les mots par des guillemets
+# Ici on met les majuscules pour faire le parallèle avec la partie java, mais 
+# elles ne sont pas utiles.
+
+DROP TABLE Reservations;
+DROP TABLE Chambres;
+DROP TABLE Hotels;
+DROP TABLE Clients;
+
+
+CREATE TABLE Clients (
+
+	id        integer,
+	prenom    VARCHAR(50),
+	nom       VARCHAR(100) NOT NULL,
+	mail      VARCHAR(256),
+	telephone VARCHAR(20),
+
+	CONSTRAINT pk_Clients PRIMARY KEY(id)
+
+);
+
+
+CREATE TABLE Hotels (
+
+	id        integer,
+	nom       VARCHAR(100),
+	adresse   text,
+	nbChambre smallint,
+
+	CONSTRAINT pk_Hotels PRIMARY KEY(id)
+
+);
+
+
+CREATE TABLE Chambres (
+
+	numeroChambre integer,
+	hotel         integer,
+	nbLitSimple   smallint DEFAULT 0,
+	nbLitDouble   smallint DEFAULT 0,
+	prix          integer,
+	gammeChambre  VARCHAR(100),
+	etage         smallint,
+
+	CONSTRAINT pk_Chambres_id PRIMARY KEY(numeroChambre,hotel),
+	CONSTRAINT fk_Hotels FOREIGN KEY (hotel) REFERENCES Hotels(id)
+
+);
+
+CREATE TABLE Reservations (
+
+	id        integer,
+	dateDebut timestamp,
+	dateFIn   timestamp,
+	client    integer,
+	chambre   integer,
+	hotel     integer,
+
+	CONSTRAINT pk_Reservations_id PRIMARY KEY(id),
+
+	CONSTRAINT fk_Clients  FOREIGN KEY (client)         REFERENCES Clients(id),
+	CONSTRAINT fk_Chambres FOREIGN KEY (chambre, hotel) REFERENCES Chambres(numeroChambre, hotel)
+
+);
