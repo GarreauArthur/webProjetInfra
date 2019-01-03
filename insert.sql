b652eda69f1b792593477ef67f7f36f8f254c17b
AdrienIzy <adrien.izydorczyk@reseau.eseo.fr>
2019-01-03 12:32:02 +0100

    Changement du script SQL

--- insert.sql
+++ insert.sql
@@ -1,20 +1,20 @@
 INSERT INTO Clients
-(id, prenom , nom      , mail            , telephone)
+(prenom , nom      , mail            , telephone)
 VALUES
-(1 , 'toto' , 'Tata'   , 'toto@yeet.com' , '0251445357'),
-(2 , 'wadu' , 'Hek'    , 'wadu@heck.hek' , '3615-hekistan'),
-(3 , 'alice', 'cagliss', 'alice@alice.fr', '0736381736'),
-(4 , 'foo'  , 'oof'    , 'foo@oof.foo'   , '0938294638')
+('toto' , 'Tata'   , 'toto@yeet.com' , '0251445357'),
+('wadu' , 'Hek'    , 'wadu@heck.hek' , '3615-hekistan'),
+('alice', 'cagliss', 'alice@alice.fr', '0736381736'),
+('foo'  , 'oof'    , 'foo@oof.foo'   , '0938294638')
 ;
 
 
 INSERT INTO Hotels
-(id, nom      , adresse                         )
+(nom      , adresse                         )
 VALUES
-(1 , 'Tothell', '29 rue des blagpatrédrol'      ),
-(2 , 'Hekel'  , '2 street of hekistant'         ),
-(3 , 'Otello' , '23 boulevard Charles Allemagne'),
-(4 , 'Hothell', '-18 avenue de yfaicho'         )
+('Tothell', '29 rue des blagpatrédrol'      ),
+('Hekel'  , '2 street of hekistant'         ),
+('Otello' , '23 boulevard Charles Allemagne'),
+('Hothell', '-18 avenue de yfaicho'         )
 ;
 
 
@@ -32,10 +32,9 @@ VALUES
 ;
 
 INSERT INTO Reservations
-(id, dateDebut            , dateFIn               , client, chambre, hotel)
+(dateDebut            , dateFIn               , client, chambre, hotel)
 VALUES
-(1 , '20181218'::timestamp, '20181219'::timestamp , 1     , 1      , 1    ),
-(2 , '20181224'::timestamp, '20181226'::timestamp , 2     , 4      , 2    ),
-(3 , '20181230'::timestamp, '20190102'::timestamp , 3     , 6      , 3    ),
-(4 , '20190123'::timestamp, '20190128'::timestamp , 4     , 8      , 4    )
-;
+('20181218'::timestamp, '20181219'::timestamp , 1     , 1      , 1    ),
+('20181224'::timestamp, '20181226'::timestamp , 2     , 4      , 2    ),
+('20181230'::timestamp, '20190102'::timestamp , 3     , 6      , 3    ),
+('20190123'::timestamp, '20190128'::timestamp , 4     , 8      , 4    )
